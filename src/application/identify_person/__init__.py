a2925d739a17365d3e690d9e771d5d80ce74f145
komorilab <komorilab@example.com>
2025-09-29 10:41:01 +0900

    feat: OAuth2認証を追加

--- src/application/identify_person/__init__.py
+++ src/application/identify_person/__init__.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Form, UploadFile
+from typing import Union
+from fastapi import FastAPI, Form, Header, HTTPException, UploadFile
 
 from application.identify_person.background_process import ApplicationIdentifyPersonBackgroundProcess
 from application.identify_person.response_parser import ApplicationIdentifyPersonResponseParser
+from service.authentication import ServiceAuthentication
 
 class ApplicationIdentifyPerson:
     @staticmethod
@@ -15,7 +17,20 @@ class ApplicationIdentifyPerson:
         camera_id: int = Form(...),
         view_id: int = Form(...),
         timestamp: str = Form(...),
+        x_header: Union[str, None] = Header(default=None),
     ) -> None:
+        if x_header is None:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+        x_header_value = x_header.split(" ")
+        if len(x_header_value) != 2:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+        if x_header_value[0] != "Bearer":
+            raise HTTPException(status_code=401, detail="Unauthorized")
+        x_header_token = x_header_value[1]
+        client_jwt_token = ServiceAuthentication.verify_jwt_token_for_client(x_header_token)
+        if client_jwt_token is None:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+
         person_crop_images = await ApplicationIdentifyPersonResponseParser.parse(
             images,
             camera_id,
