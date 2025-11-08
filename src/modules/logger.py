47caa6ae9021a2584d27c93e67fd8f4d983d5324
komorilab <komorilab@example.com>
2025-11-08 12:05:15 +0900

    feat: error handling in logger

--- src/modules/logger.py
+++ src/modules/logger.py
@@ -1,32 +1,43 @@
 import logging
+import os
 from entities.environment.storage import EntityEnvironmentStorage
 from datetime import datetime
 
 class ModuleLogger:
-    _logger: logging.Logger
+    _logger: logging.Logger | None = None
     LOG_FILE_PATH = "{storage_path}/logs/{file_name}.log"
 
     def __init__(self, environment_storage: EntityEnvironmentStorage):
+        self.path = self.LOG_FILE_PATH.format(
+            storage_path=environment_storage.path,
+            file_name=datetime.now().strftime("%Y%m%d%H%M%S")
+        )
+        os.makedirs(os.path.dirname(self.path), exist_ok=True)
         if self._logger is None:
             self._logger = logging.getLogger()
             self._logger.setLevel(logging.INFO)
             self._logger.addHandler(logging.StreamHandler())
             self._logger.addHandler(logging.FileHandler(
-                    self.LOG_FILE_PATH.format(
-                        storage_path=environment_storage.path,
-                        file_name=datetime.now().strftime("%Y%m%d%H%M%S")
-                    ),
+                    self.path,
                 )
             )
 
     def info(self, message: str):
+        if self._logger is None:
+            raise ValueError("Logger is not initialized")
         self._logger.info(message)
 
     def error(self, message: str):
+        if self._logger is None:
+            raise ValueError("Logger is not initialized")
         self._logger.error(message)
 
     def warning(self, message: str):
+        if self._logger is None:
+            raise ValueError("Logger is not initialized")
         self._logger.warning(message)
 
     def debug(self, message: str):
+        if self._logger is None:
+            raise ValueError("Logger is not initialized")
         self._logger.debug(message)
