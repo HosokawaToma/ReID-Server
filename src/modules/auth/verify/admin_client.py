ac2b58d3b5026faaa0f0c2045b9846d70b53e7b8
komorilab <komorilab@example.com>
2025-11-09 21:20:02 +0900

    fix: error message

--- src/modules/auth/verify/admin_client.py
+++ src/modules/auth/verify/admin_client.py
@@ -15,21 +15,21 @@ class ModuleAuthVerifyAdminClient:
         try:
             payload = jwt.decode(token, self.secret_key, algorithms=[self.algorithm])
         except Exception:
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not payload:
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not isinstance(payload, dict):
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not payload.get(self.ADMIN_CLIENT_ID_KEY_OF_PAYLOAD):
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         admin_client_id = payload.get(self.ADMIN_CLIENT_ID_KEY_OF_PAYLOAD)
         if not isinstance(admin_client_id, str):
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not payload.get(self.EXPIRE_TIME_KEY_OF_PAYLOAD):
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         expire_time = payload.get(self.EXPIRE_TIME_KEY_OF_PAYLOAD)
         if not isinstance(expire_time, int):
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if expire_time < time.time() + self.expire_minutes * 60:
             raise Exception("Token expired")
         return EntityJWTAdminClient(admin_client_id)
--- src/modules/auth/verify/camera_client.py
+++ src/modules/auth/verify/camera_client.py
@@ -17,31 +17,31 @@ class ModuleAuthVerifyCameraClient:
         try:
             payload = jwt.decode(token, self.secret_key, algorithms=[self.algorithm])
         except Exception:
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not payload:
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not isinstance(payload, dict):
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         camera_client_id = payload.get(self.CAMERA_CLIENT_ID_KEY_OF_PAYLOAD)
         camera_id = payload.get(self.CAMERA_ID_KEY_OF_PAYLOAD)
         view_id = payload.get(self.VIEW_ID_KEY_OF_PAYLOAD)
         expire_time = payload.get(self.EXPIRE_TIME_KEY_OF_PAYLOAD)
         if not camera_client_id:
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not isinstance(camera_client_id, str):
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not camera_id:
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not isinstance(camera_id, int):
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not view_id:
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not isinstance(view_id, int):
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not expire_time:
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if not isinstance(expire_time, int):
-            raise Exception("Invalid authorization token")
+            raise Exception("Invalid token")
         if expire_time < time.time() + self.expire_minutes * 60:
             raise Exception("Token expired")
         return EntityJWTCameraClient(camera_client_id, camera_id, view_id)
